2e11b1b07a223a2914ff33841aeb4bd663c77226
Rodolfo Quesada Amores <fofo.q.a@hotmail.com>
2017-05-29 20:27:32 -0600

    se crea proyecto de logica de negocio

---
+++ Ulatina.PrograAvanzada-BLAplicacionWeb/Ulatina.PrograAvanzada-BLAplicacionWeb/Dominio/Acciones/ObtenerFechaActual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada_BLAplicacionWeb.Dominio.Acciones
+{
+    public class ObtenerFechaActual
+    {
+        public string ObtengaLaFecha()
+
+        {
+            //Ulatina,PrograAvanzada.BLAplicacionWeb.Dominio.Especificaciones.ObtengaFecha la //
+
+
+            // Especificaciones.ObtengaFecha laEspecificacion = new Especificaciones.ObtengaFecha();//
+
+            var laEspecificacion = new Especificaciones.ObtengaFecha();
+            var laFecha = laEspecificacion.FormatearUnaFecha(DateTime.Now, "dd/mm/yyyy");
+            return laFecha;
+
+
+        }
+
+
+    }
+}
---
+++ Ulatina.PrograAvanzada-BLAplicacionWeb/Ulatina.PrograAvanzada-BLAplicacionWeb/Dominio/Especificaciones/ObtengaFecha.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada_BLAplicacionWeb.Dominio.Especificaciones
+{
+    public class ObtengaFecha
+    {
+        public string FormatearUnaFecha (DateTime lafecha, string elFormato)
+
+
+        {
+            return "Hello world";
+
+
+        }
+            
+            
+            }
+}
